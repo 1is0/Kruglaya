7b7762b4cd15207e0c7c9f9ca5756a9538359656
Nastiona-Slastiona <ariaignis.dragon@gmail.com>
2020-04-26 21:32:53 +0300

    Add files via upload

---
+++ Lab_CSharp/Lab7/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+
+namespace LabSeven
+{
+    class Program
+    {
+
+        public static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Ошибка, введите данные еще раз. \n");
+            }
+            return a;
+        }
+        public static RationalNumber Number()
+        {
+            Console.WriteLine("Введите рациональное число: ");
+            double x;
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Вы ввели некорректное число, попробуйте еще раз.\n");
+            }
+            return new RationalNumber(x);
+        }
+        public static RationalNumber Fraction()
+        {
+
+            string number;
+            string integer = "";
+            string natural = "";
+            Console.WriteLine("Введите число: ");
+            number = Console.ReadLine();
+
+            char[] array = number.ToCharArray();
+            for (int i = 0, index = number.IndexOf('/'); i != index; i++)
+            {
+                integer += array[i];
+            }
+            for (int i = number.IndexOf('/') + 1; i < number.Length; i++)
+            {
+                natural += array[i];
+            }
+
+            int.TryParse(integer, out int n);
+            int.TryParse(natural, out int m);
+
+            return new RationalNumber(n, m);
+
+        }
+        public static RationalNumber Create()
+        {
+            int choice;
+            Console.WriteLine("В каком виде будете вводить число: 0 - Дробь / 1 - С плавающей точкой \n");
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > 1)
+            {
+                Console.WriteLine("Ошибка, введите данные еще раз. \n");
+            }
+            if (choice == 1)
+            {
+                return Number();
+            }
+            else
+            {
+                return Fraction();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            RationalNumber num1 = Create();
+
+            Console.WriteLine("Введите второе число");
+            RationalNumber num2 = Create();
+
+            Console.Clear();
+            Console.WriteLine("Перое число:\n" + num1);
+            Console.WriteLine("ВТорое число:\n" + num2);
+            Console.WriteLine($"Сравнение чисел: {num1.Fraction()} > {num2.Fraction()} = " + (num1 > num2));
+            Console.WriteLine($"{num1.Number()} > {num2.Number()} = " + (num1 > num2) + "\n");
+            Console.WriteLine($"{num1.Fraction()} < {num2.Fraction()} = " + (num1 < num2));
+            Console.WriteLine($"{num1.Number()} < {num2.Number()} = " + (num1 < num2) + "\n");
+            Console.WriteLine($"{num1.Fraction()} == {num2.Fraction()} = " + (num1 == num2));
+            Console.WriteLine($"{num1.Number()} == {num2.Number()} = " + (num1 == num2) + "\n");
+            Console.WriteLine($"{num1.Fraction()} != {num2.Fraction()} = " + (num1 != num2));
+            Console.WriteLine($"{num1.Number()} != {num2.Number()} = " + (num1 != num2) + "\n");
+            Console.WriteLine($"Сложение: {num1.Fraction()} + {num2.Fraction()} = " + (num1 + num2).Fraction());
+            Console.WriteLine($"   {num1.Number()} + {num2.Number()} = " + (num1 + num2).Number() + "\n");
+            Console.WriteLine($"Вычитание: {num1.Fraction()} - {num2.Fraction()} = " + (num1 - num2).Fraction());
+            Console.WriteLine($"   {num1.Number()} - {num2.Number()} = " + (num1 - num2).Number() + "\n");
+            Console.WriteLine($"Умножение: {num1.Fraction()} * {num2.Fraction()} = " + (num1 * num2).Fraction());
+            Console.WriteLine($"   {num1.Number()} * {num2.Number()} = " + (num1 * num2).Number() + "\n");
+            Console.WriteLine($"Остаток от деления: {num1.Fraction()} % {num2.Fraction()} = " + (num1 % num2).Fraction());
+            Console.WriteLine($"   {num1.Number()} % {num2.Number()} = " + (num1 % num2).Number() + "\n");
+            if (num2 != 0)
+            {
+                Console.WriteLine($"Деление: {num1.Fraction()} / {num2.Fraction()} = " + (num1 / num2).Fraction());
+                Console.WriteLine($"   {num1.Number()} / {num2.Number()} = " + (num1 / num2).Number() + "\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_CSharp/Lab7/RationalNumber.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+
+namespace LabSeven
+{
+    class RationalNumber : IComparable, IEquatable<RationalNumber>
+    {
+        public int Numerator { get; set; }
+        public int Deniminator { get; set; }
+ 
+        double number;
+
+        
+
+        public RationalNumber()
+        {
+            Numerator = 1;
+            Deniminator = 1;
+            number = 1;
+        }
+        public RationalNumber(int n, int m)
+        {
+            Numerator = n;
+            Deniminator = m;
+            number = (double)n / m;
+        }
+        public RationalNumber(double num)
+        {
+            int pow_10 = -1;
+            number = num;
+            if (num % 10 != 0)
+            {
+                while (num % 10 != 0 || Math.Abs(num) < 1)
+                {
+                    num *= 10;
+                    pow_10++;
+                }
+                num /= 10;
+                Numerator = (int)num;
+                Deniminator = (int)Math.Pow(10, pow_10);
+            }
+            else
+            {
+                Numerator = (int)num;
+                Deniminator = 1;
+
+            }
+        }
+
+        public static RationalNumber operator +(RationalNumber x, RationalNumber y)
+        {
+            RationalNumber answer = new RationalNumber();
+            answer.Numerator = x.Numerator * y.Deniminator + y.Numerator * x.Deniminator;
+            answer.Deniminator = x.Deniminator * y.Deniminator;
+            answer.number = x.number + y.number;
+
+            return answer;
+        }
+
+        public static RationalNumber operator -(RationalNumber x, RationalNumber y)
+        {
+            RationalNumber answer = new RationalNumber();
+            answer.Numerator = x.Numerator * y.Deniminator - y.Numerator * x.Deniminator;
+            answer.Deniminator = x.Deniminator * y.Deniminator;
+            answer.number = x.number - y.number;
+
+            return answer;
+        }
+
+        public static RationalNumber operator *(RationalNumber x, RationalNumber y)
+        {
+            RationalNumber answer = new RationalNumber();
+            answer.Numerator = x.Numerator * y.Numerator;
+            answer.Deniminator = x.Deniminator * y.Deniminator;
+            answer.number = x.number * y.number;
+
+            return answer;
+        }
+
+        public static RationalNumber operator /(RationalNumber x, RationalNumber y)
+        {
+            RationalNumber answer = new RationalNumber();
+            answer.Numerator = x.Numerator * y.Deniminator;
+            answer.Deniminator = y.Numerator * x.Deniminator;
+            answer.number = x.number / y.number;
+
+            return answer;
+        }
+        public static RationalNumber operator %(RationalNumber x, RationalNumber y)
+        {
+            if (x < y)
+            {
+                return x;
+            }
+            else
+            {
+                double remainder = x.number - ((int)(x.number / y.number)) * x.number;
+                RationalNumber answer = new RationalNumber(remainder);
+                return answer;
+            }
+        }
+        public static RationalNumber operator ++(RationalNumber x)
+        {
+            x.Numerator += x.Deniminator;
+            x.number++;
+            return x;
+        }
+        public static RationalNumber operator --(RationalNumber x)
+        {
+            x.Numerator -= x.Deniminator;
+            x.number--;
+            return x;
+        }
+        public static bool operator >(RationalNumber x, RationalNumber y) => x.number > y.number;
+        public static bool operator >=(RationalNumber x, RationalNumber y) => x.number >= y.number;
+        public static bool operator <(RationalNumber x, RationalNumber y) => x.number < y.number;
+        public static bool operator <=(RationalNumber x, RationalNumber y) => x.number <= y.number;
+        public static bool operator !=(RationalNumber x, RationalNumber y) => !x.Equals(y);
+        public static bool operator ==(RationalNumber x, RationalNumber y) => x.Equals(y);
+
+        public string  Number() =>$"{number}";
+        public string Fraction() => $"{Numerator}/{Deniminator}";
+        public override string ToString()
+        {
+            return $"Rational Number: " + Number() + "nRational number as fraction:" + Fraction();
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is RationalNumber)
+            {
+                return base.Equals((RationalNumber)other);
+            }
+            return false;
+        }
+        public bool Equals(RationalNumber num)
+        {
+            if (this.number == num.number) return true;
+            else return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            if (this.number > ((RationalNumber)obj).number) return 1;
+            else if (this.number < ((RationalNumber)obj).number) return -1;
+            else return 0;
+        }
+
+        public static implicit operator double(RationalNumber x) => x.number;
+        public static explicit operator int(RationalNumber x) => (int)x.number;
+        public static explicit operator RationalNumber(int x)
+        {
+            RationalNumber result = new RationalNumber(x);
+            return result;
+        }
+
+    }
+}
